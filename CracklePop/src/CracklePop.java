8305b89ee107a66b9608c29736908da142fb3cb4
ShlomitA <ShlomitA@Shlomita-Deskto.tufin.com>
2014-07-30 13:04:14 +0300

    CracklePop implementation

--- CracklePop/src/CracklePop.java
+++ CracklePop/src/CracklePop.java
@@ -1,24 +1,37 @@
 /**
- * 
- */
-
-/**
  * @author Shlomit Argov
- *	main prints out all numbers between 1 and MAX_NUM.
- *	If a number is divisible by 3, it prints Crackle instead of the number.
- *	If a number is divisible by 5, it prints Pop instead of the number.
- *	If a number is divisible by both 3 and 5, it prints CracklePop instead of the number.
- *
  */
 public class CracklePop {
 	private static final int MAX_NUM = 100;
 	private static final String CRACKLE = "Crackle";
 	private static final String POP = "Pop";
-	
+
+	/**
+	 * main prints out all numbers between 1 and MAX_NUM. If a number is
+	 * divisible by 3, it prints Crackle instead of the number. If a number is
+	 * divisible by 5, it prints Pop instead of the number. If a number is
+	 * divisible by both 3 and 5, it prints CracklePop instead of the number.
+	 * 
+	 */
 	public static void main(String[] args) {
-	
-		
 
+		for (int i = 1; i <= MAX_NUM; i++) {
+			if ((i % 3) == 0) 
+			{
+				System.out.print(CRACKLE);
+				if ((i % 5) == 0) 
+				{
+					System.out.println(POP);
+				} else {
+					System.out.println();
+				}
+			} else if ((i % 5) == 0) 
+			{
+				System.out.println(POP);
+			} else 
+			{
+				System.out.println(i);
+			}
+		}
 	}
-
 }
